ad2b202d4ca409609e38d18178d8e21c33eb45ef
liuhong <411455825@qq.com>
2013-03-14 02:36:13 -0700

    提交新的配置界面代码

---
+++ config/js/main.js
@@ -0,0 +1,21 @@
+﻿/**
+ * Class: CF_Main
+ * 配置界面。
+ */
+(function(){
+    function A(){
+        var t = this;
+        t.createToolbar();
+    }
+    var B = A.prototype;
+    /**
+     * Method: createToolbar
+     * 创建配置工具栏。
+     */
+    B.createToolbar = function(){
+        var body;
+
+        body = $("#toolbar")
+    }
+    new A();
+})()
---
+++ config/js/title.js
@@ -0,0 +1,43 @@
+﻿/**
+ * Class: SM_Tile
+ * 界面的标题栏。
+ */
+(function(){
+    function A(){
+        var t = this;
+        t.create();
+    }
+    var B = A.prototype;
+    /**
+     * Method: create
+     * 创建title区域。
+     */
+    B.create = function(){
+        var body = $("#sm_title"),d1,d2;
+        if(body){
+            body.css({
+                "height":"80px",
+                "background":"#fff",
+                "border-bottom":"2px solid #29a3e2"
+            });
+
+            d1 = $("<div>")
+                .css({
+                    "margin":"0 auto",
+                    "width":"960px"
+                })
+                .appendTo(body);
+
+            d2 = $("<div>")
+                .css({
+                    "height":"48px",
+                    "width":"311px",
+                    "background": "url(../images/init/logo.png) no-repeat",
+                    "position":"relative",
+                    "top":"21px"
+                })
+                .appendTo(d1);
+        }
+    }
+    new A();
+})()
